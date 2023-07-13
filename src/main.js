2c1ae7b661aba7f3748206a50889a0d5b8fc77ea
chaerhin <cofls8929@naver.com>
2023-07-14 01:59:13 +0900

    + vuetify-dialog import
    + vuetify-dialog import

--- src/main.js
+++ src/main.js
@@ -8,11 +8,21 @@ import vuetify from './plugins/vuetify'
 ///import firebase from 'firebase/compat/app'
 ///import '@/datasources/firebase'
 
+// //Vuetify-dialog
+// import VuetifyDialog from 'vuetify-dialog'
+// import 'vuetify-dialog/dist/vuetify-dialog.css'
+
+// Vue.use(VuetifyDialog, {
+//   context: {
+//     vuetify
+//   }
+// })
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
