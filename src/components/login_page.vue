8e0ad854badebcd8494b61f958d74aba9433e116
Park Chaerhin <127855706+park-chaerhin@users.noreply.github.com>
2023-07-17 13:51:22 +0900

    로그인 버튼 디자인 수정

--- src/components/login_page.vue
+++ src/components/login_page.vue
@@ -10,21 +10,18 @@
                     <v-text-field name="email" label="이메일" type="email" v-model="sEmail" required :rules="[rule.required, rule.email]"></v-text-field>
                     <v-text-field name="password" label="비밀번호" type="password" v-model="sPassword" required :rules="[rule.required]"></v-text-field>
 
-                    <v-btn type="submit" v-if="!fnGetLoading" color="#2c38a8" dark>로그인</v-btn>
-                    <v-progress-circular v-if="fnGetLoading" indeterminate :width="7" :size="70" color="grey lighten-1"></v-progress-circular>
-                </form>
-            </v-col>
-            <!-- <v-col offset="3" cols="6" class="text-center mt-5">
-            <v-btn to="/login" block color="2c38a8" large dark>
-                <v-icon left>mdi-email</v-icon>이메일 로그인
-            </v-btn>
-            </v-col>
- -->
-            <v-col offset="3" cols="6" class="text-center mt-5">
+                    <v-col offset="3" cols="6" class="text-center mt-5">
+                        <v-btn type="submit" block outlined color="2c38a8" large  @click="redirectToLink">
+                            <v-icon left color="#2c38a8">mdi-email</v-icon>이메일 로그인
+                        </v-btn>
+                    </v-col>
+                    <v-col offset="3" cols="6" class="text-center mt-5">
             <!--구글 계정 로그인 버튼 표시 및 처리-->
                 <v-btn @click="fnDoGoogleLogin_Popup" block outlined color="#2c38a8" large dark>
-                    <v-icon left color="red">mdi-google</v-icon>구글 로그인
+                    <v-icon left color="#2c38a8">mdi-google</v-icon>구글 로그인
                 </v-btn>
+            </v-col>
+                </form>
             </v-col>
         </v-row>
     </v-container>
@@ -48,17 +45,15 @@
                 this.$store.dispatch('fnDoGoogleLogin_Popup')
             },
             fnDoLogin(){
-                this.$store.dispatch('fnDoLogin', {  /// 위의 function에서 호추한 fnDoLogin함수랑 / this.$store.의 fnDoLogin는 다른 함수
+                this.$store.dispatch('fnDoLogin', {  
                     pEmail: this.sEmail,
                     pPassword: this.sPassword
                 })
+            },
+            redirectToLink(){
+                alert('로그인 되었습니다.')
+                this.$router.push({ name: "intro_page" });
             }
         },
-        computed: {
-            fnGetLoading() {
-          //시간지연상태 스토어에서 읽어서 반환
-            return this.$store.getters.fnGetLoading
-        },
-    },
 }
 </script>
